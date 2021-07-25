9b313fc9636640005e171c32f56f58496b32ad31
sayyedmaroof <sayyedmaroof9@gmail.com>
2021-07-25 22:26:46 +0530

    Fixed Image upload issue

--- controllers/storePost.js
+++ controllers/storePost.js
@@ -3,7 +3,6 @@ const Post = require('../database/models/post')
 
 module.exports = async (req, res) => {
     const { image } = req.files
-    image.name = Date.now().toString()
 
     image.mv(path.resolve(__dirname, '..', 'public/posts', image.name), error => {
         Post.create({
